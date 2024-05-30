9a2f4e300dea7dacadc084b1ba57de728db7f856
Philip Hayes <philiphayes9@gmail.com>
2024-05-30 14:34:23 -0700

    ffi: cleanup: put public FFI fns at top of file

--- src/native_zxing.cpp
+++ src/native_zxing.cpp
@@ -17,7 +17,52 @@ using namespace ZXing;
 using namespace std;
 using std::chrono::steady_clock;
 
+// Forward declare some impls
+CodeResult _readBarcode(const DecodeBarcodeParams& params);
+CodeResults _readBarcodes(const DecodeBarcodeParams& params);
+EncodeResult _encodeBarcode(const EncodeBarcodeParams& params);
+
+//
+// Public, exported FFI functions
+//
+
+extern "C"
+{
+    FUNCTION_ATTRIBUTE
+    void setLogEnabled(bool enable)
+    {
+        setLoggingEnabled(enable);
+    }
+
+    FUNCTION_ATTRIBUTE
+    char const *version()
+    {
+        // return ZXING_VERSION_STR; // TODO: Not working on iOS for now
+        return "2.2.1";
+    }
+
+    FUNCTION_ATTRIBUTE
+    struct CodeResult readBarcode(struct DecodeBarcodeParams params)
+    {
+        return _readBarcode(params);
+    }
+
+    FUNCTION_ATTRIBUTE
+    struct CodeResults readBarcodes(struct DecodeBarcodeParams params)
+    {
+        return _readBarcodes(params);
+    }
+
+    FUNCTION_ATTRIBUTE
+    struct EncodeResult encodeBarcode(struct EncodeBarcodeParams params)
+    {
+        return _encodeBarcode(params);
+    }
+}
+
+//
 // Helper functions
+//
 
 ImageView createCroppedImageView(const struct DecodeBarcodeParams &params)
 {
@@ -94,95 +139,78 @@ int elapsed_ms(const steady_clock::time_point &start)
     return chrono::duration_cast<chrono::milliseconds>(duration).count();
 }
 
-extern "C"
+//
+// FFI impls
+//
+
+CodeResult _readBarcode(const DecodeBarcodeParams& params)
 {
+    auto start = steady_clock::now();
 
-    FUNCTION_ATTRIBUTE
-    void setLogEnabled(bool enable)
+    ImageView image = createCroppedImageView(params);
+    ReaderOptions hints = createReaderOptions(params);
+    Result result = ReadBarcode(image, hints);
+
+    int duration = elapsed_ms(start);
+    platform_log("Read Barcode in: %d ms\n", duration);
+    return codeResultFromResult(result, duration, params.width, params.height);
+}
+
+CodeResults _readBarcodes(const DecodeBarcodeParams& params)
+{
+    auto start = steady_clock::now();
+
+    ImageView image = createCroppedImageView(params);
+    ReaderOptions hints = createReaderOptions(params);
+    Results results = ReadBarcodes(image, hints);
+
+    int duration = elapsed_ms(start);
+    platform_log("Read Barcode in: %d ms\n", duration);
+
+    if (results.empty())
     {
-        setLoggingEnabled(enable);
+        return CodeResults{0, nullptr, duration};
     }
 
-    FUNCTION_ATTRIBUTE
-    char const *version()
+    auto *codes = new struct CodeResult[results.size()];
+    int i = 0;
+    for (const auto &result : results)
     {
-        // return ZXING_VERSION_STR; // TODO: Not working on iOS for now
-        return "2.2.1";
+        codes[i] = codeResultFromResult(result, duration, params.width, params.height);
+        i++;
+    }
+    return CodeResults{i, codes, duration};
+}
+
+EncodeResult _encodeBarcode(const EncodeBarcodeParams& params)
+{
+    auto start = steady_clock::now();
+
+    struct EncodeResult result = {0, params.contents, params.format, nullptr, 0, nullptr};
+    try
+    {
+        auto writer = MultiFormatWriter(BarcodeFormat(params.format)).setMargin(params.margin).setEccLevel(params.eccLevel).setEncoding(CharacterSet::UTF8);
+        auto bitMatrix = writer.encode(params.contents, params.width, params.height);
+        auto matrix = ToMatrix<uint8_t>(bitMatrix);
+
+        // We need to return an owned pointer across the ffi boundary. Copy
+        // the output (again).
+        auto length = matrix.size();
+        auto *data = new uint8_t[length];
+        std::copy(matrix.begin(), matrix.end(), data);
+
+        result.length = length;
+        result.data = data;
+        result.isValid = true;
+    }
+    catch (const exception &e)
+    {
+        platform_log("Can't encode text: %s\nError: %s\n", params.contents, e.what());
+        result.error = new char[strlen(e.what()) + 1];
+        strcpy(result.error, e.what());
     }
 
-    FUNCTION_ATTRIBUTE
-    struct CodeResult readBarcode(struct DecodeBarcodeParams params)
-    {
-        auto start = steady_clock::now();
-
-        ImageView image = createCroppedImageView(params);
-        ReaderOptions hints = createReaderOptions(params);
-        Result result = ReadBarcode(image, hints);
-
-        int duration = elapsed_ms(start);
-        platform_log("Read Barcode in: %d ms\n", duration);
-        return codeResultFromResult(result, duration, params.width, params.height);
-        ;
-    }
-
-    FUNCTION_ATTRIBUTE
-    struct CodeResults readBarcodes(struct DecodeBarcodeParams params)
-    {
-        auto start = steady_clock::now();
-
-        ImageView image = createCroppedImageView(params);
-        ReaderOptions hints = createReaderOptions(params);
-        Results results = ReadBarcodes(image, hints);
-
-        int duration = elapsed_ms(start);
-        platform_log("Read Barcode in: %d ms\n", duration);
-
-        if (results.empty())
-        {
-            return CodeResults{0, nullptr, duration};
-        }
-
-        auto *codes = new struct CodeResult[results.size()];
-        int i = 0;
-        for (const auto &result : results)
-        {
-            codes[i] = codeResultFromResult(result, duration, params.width, params.height);
-            i++;
-        }
-        return CodeResults{i, codes, duration};
-    }
-
-    FUNCTION_ATTRIBUTE
-    struct EncodeResult encodeBarcode(struct EncodeBarcodeParams params)
-    {
-        auto start = steady_clock::now();
-
-        struct EncodeResult result = {0, params.contents, params.format, nullptr, 0, nullptr};
-        try
-        {
-            auto writer = MultiFormatWriter(BarcodeFormat(params.format)).setMargin(params.margin).setEccLevel(params.eccLevel).setEncoding(CharacterSet::UTF8);
-            auto bitMatrix = writer.encode(params.contents, params.width, params.height);
-            auto matrix = ToMatrix<uint8_t>(bitMatrix);
-
-            // We need to return an owned pointer across the ffi boundary. Copy
-            // the output (again).
-            auto length = matrix.size();
-            auto *data = new uint8_t[length];
-            std::copy(matrix.begin(), matrix.end(), data);
-
-            result.length = length;
-            result.data = data;
-            result.isValid = true;
-        }
-        catch (const exception &e)
-        {
-            platform_log("Can't encode text: %s\nError: %s\n", params.contents, e.what());
-            result.error = new char[strlen(e.what()) + 1];
-            strcpy(result.error, e.what());
-        }
-
-        int duration = elapsed_ms(start);
-        platform_log("Encode Barcode in: %d ms\n", duration);
-        return result;
-    }
+    int duration = elapsed_ms(start);
+    platform_log("Encode Barcode in: %d ms\n", duration);
+    return result;
 }
